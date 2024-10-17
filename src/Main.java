ab9ceae6faa27be65d237dcef23b8e2e2535a3ab
smarK99 <santikb99@gmail.com>
2024-10-17 13:44:31 -0300

    algoritmo chequea solo en filas y columnas

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+
+
+
     public static void main(String[] args) {
 
-        List<String> dna = new ArrayList<>(List.of("ACGTC", "CGTAG", "CTGAA", "TAGCG", "AAAGA"));
+        List<String> dna = new ArrayList<>(List.of("ACGTA", "CGTAC", "CTGAC", "TAGCC", "AAACC"));
         int n = dna.size();
         char[][] matrizDna = new char[n][n];
 
@@ -35,45 +39,45 @@ public class Main {
         }
 
         //Chequear el adn por filas
+
         for (int i = 0; i < matrizDna.length; i++) {
             int contador = 1;
             //La cuenta empieza desde n+1, es decir desde el elemento 0 1
-            for (int j = 1; j < matrizDna[i].length; j++) {
+            for (int j = 1; j < matrizDna.length; j++) {
                 //Compara el elemento n con su anterior n-1
                 if (matrizDna[i][j] == matrizDna[i][j - 1]) {
                     contador++;
-                    // Si el contador llega a 4, se ha encontrado una secuencia válida
                     if (contador >= 4) {
                         System.out.println("Secuencia encontrada en la fila " + (i + 1) + ": " + matrizDna[i][j]);
                         System.out.println("Es mutante");
                         break;
                     }
                 } else {
-                    contador = 1; // Reinicia el contador si los caracteres no son iguales
+                    contador = 1;
                 }
             }
         }
 
-
         //Chequear el adn por columnas
 
-        //Extraigo las columnas y las guardo en un array
-//        List<String> cols = new ArrayList<>();
+        for (int i = 0; i < matrizDna.length; i++) {
+            int contador = 1;
+            //La cuenta empieza desde n+1, es decir desde el elemento 0 1
+            for (int j = 1; j < matrizDna.length; j++) {
+                //Compara el elemento n con su anterior n-1
+                if (matrizDna[j][i] == matrizDna[j-1][i]) {
+                    contador++;
+                    if (contador >= 4) {
+                        System.out.println("Secuencia encontrada en la columna " + (j + 1) + ": " + matrizDna[j][i]);
+                        System.out.println("Es mutante");
+                        break;
+                    }
+                } else {
+                    contador = 1;
+                }
+            }
+        }
 
-//        for (int c=0; c < n; c++) {
-//            for (int f = 0; f < n; f++) {
-//                if (f == 0){
-//                    cols.add(String.valueOf(matrizDna[f][c]));
-//                }else{
-//                    break;
-//                }
-//            }
-//        }
-//
-//        //Recorro la columna
-//        for (String col : cols){
-//            System.out.println(col);
-//        }
 
 
 
